7c5d56dfbaeae1449d7bb8af3dd87543bb0ba5d5
Abel Kihara <abelkihara8@gmail.com>
2026-02-09 01:43:44 +0300

    Fix_PORT_for_Cloud_Run

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const db = new Database('students.db');
 
 app.use(cors());
